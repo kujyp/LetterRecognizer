57b0bcca960a27c1d7e2364595f2edc69623cec9
pjo901018 <pjo901018@korea.ac.kr>
2017-02-24 09:25:04 +0900

    저장된 학습값 불러오고 초기화되는문제 수정

--- src/predictor.py
+++ src/predictor.py
@@ -9,25 +9,24 @@ from train_model import placeholder_inputs, graph_model, calcul_loss, training, 
 saverflag = False
 
 class Predictor:
-    sess = tf.Session()
     def __init__(self,HYPARMS):
         self.init(HYPARMS)
 
     def predict(self, img):
         return self.sess.run(self.output, feed_dict = {self.placebundle.x: img,
-                                  self.placebundle.keep_prob: 1})
+                                                       self.placebundle.keep_prob: 1})
 
     def init(self,HYPARMS):
-        sess = tf.Session()
+        self.sess = tf.Session()
         self.placebundle = placeholder_inputs(1)
 
         self.init = tf.initialize_all_variables()
         self.saver = tf.train.Saver()
         self.sess.run(self.init)
 
-        self.ckpt = tf.train.get_checkpoint_state(HYPARMS.ckpt_dir)
-        self.saver.restore(sess, self.ckpt.model_checkpoint_path)
-
         self.logits = graph_model(self.placebundle)
         self.output = tf.argmax(self.logits,1)
+
+        self.ckpt = tf.train.get_checkpoint_state(HYPARMS.ckpt_dir)
+        self.saver.restore(self.sess, self.ckpt.model_checkpoint_path)
 
